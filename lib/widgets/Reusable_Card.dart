8f63f7d8ffedf2626d15ec3be7ed0a72151f0532
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-09 02:36:13 +0530

    9-april commit32

--- lib/widgets/Reusable_Card.dart
+++ lib/widgets/Reusable_Card.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
+  final Widget CardChild;
+  final Color color;
+  final Function onPress;
   const ReusableCard({super.key});
 
   @override
