5cb7af235f71fe086b45d638592fe0eec015b430
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-09 02:44:53 +0530

    9-april commit34

--- lib/widgets/Reusable_Card.dart
+++ lib/widgets/Reusable_Card.dart
@@ -12,11 +12,14 @@ class ReusableCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: CardChild,
-      margin: EdgeInsets.all(15),
-      decoration:
-          BoxDecoration(color: color, borderRadius: BorderRadius.circular(10)),
+    return GestureDetector(
+      onTap: () => onPress(),
+      child: Container(
+        child: CardChild,
+        margin: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            color: color, borderRadius: BorderRadius.circular(10)),
+      ),
     );
   }
 }
