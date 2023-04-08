628002c64ec0f4888f32d2f6ac5f0cb9711b0a5a
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-09 03:17:39 +0530

    9-april commit36

--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -1,4 +1,7 @@
+import 'package:bmi_calci/widgets/Icon_Content.dart';
+import 'package:bmi_calci/widgets/Reusable_Card.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,6 +17,34 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
         centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: ReusableCard(
+                  CardChild: IconContent(
+                    icon: FontAwesomeIcons.mars,
+                    label: 'MALE',
+                  ),
+                  color: Colors.pink,
+                  onPress: () {},
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  CardChild: IconContent(
+                    icon: FontAwesomeIcons.mars,
+                    label: 'MALE',
+                  ),
+                  color: Colors.pink,
+                  onPress: () {},
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
