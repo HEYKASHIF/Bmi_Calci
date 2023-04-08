10b413d5f0ce998e4fec3f502f8a0b83ff6d40b3
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-09 03:01:49 +0530

    9-april commit35

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calci/pages/Home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,8 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue[900],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
