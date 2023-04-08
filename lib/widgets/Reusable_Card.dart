d3e3e812990311a79f378d5738f9896e8f24bea7
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-09 02:42:40 +0530

    9-april commit33

--- lib/widgets/Reusable_Card.dart
+++ lib/widgets/Reusable_Card.dart
@@ -4,10 +4,19 @@ class ReusableCard extends StatelessWidget {
   final Widget CardChild;
   final Color color;
   final Function onPress;
-  const ReusableCard({super.key});
+  const ReusableCard(
+      {required this.CardChild,
+      required this.color,
+      required this.onPress,
+      super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: CardChild,
+      margin: EdgeInsets.all(15),
+      decoration:
+          BoxDecoration(color: color, borderRadius: BorderRadius.circular(10)),
+    );
   }
 }
