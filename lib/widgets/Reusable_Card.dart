da819a3986c4e66d9ef2a90da47c13af2d554c27
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-09 02:04:52 +0530

    9-april commit31

---
+++ lib/widgets/Reusable_Card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
