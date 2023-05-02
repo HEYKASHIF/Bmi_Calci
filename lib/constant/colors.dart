8e70cbe0aefb91d938021a7af54186e29de96a25
HEYKASHIF <kashifmohd269@gmail.com>
2023-05-02 05:36:05 +0530

    2-april commit2

--- lib/constant/colors.dart
+++ lib/constant/colors.dart
@@ -1,1 +1,7 @@
 import 'package:flutter/material.dart';
+
+const ActiveCardColor = Color(0xFF1D1E33);
+
+const InactiveCardColor = Color(0xFF111328);
+
+const BottomContainerColor = Color(0xFFEB1555);
